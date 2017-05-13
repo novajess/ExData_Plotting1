e2bf54df4a808855201bba1eaa1a8b7da8a59556
novajess <jessicarnovak@gmail.com>
2017-05-13 10:48:34 -0700

    Add files via upload
    Updated 2017

--- plot1.r
+++ plot1.r
@@ -1,8 +1,17 @@
-data <- read.table('C:\\Users\\Jessica\\Documents\\Personal\\household_power_consumption.txt', 
-                   sep=";",header=TRUE)
-head(data)
+dataseturl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataseturl, "power.zip")
+unzip("power.zip", exdir = "power")
+list.files("power", full.names = TRUE)
 
-data2 <- data[which(data[,1] %in% c("1/2/2007", "2/2/2007")),]
+data <- read.table("power/household_power_consumption.txt", sep=";",header=TRUE)
+head(data)
+data2 <- data[which(data$Date %in% c("1/2/2007", "2/2/2007")),]
+head(data2)
+
+data2$Date <- as.Date(as.character(data2$Date), format = "%d/%m/%Y")
+data2$Time <- strptime(as.character(data$Time), format = "%H:%M:%S")
+
+data2 <- data[which(data$Date %in% c("2007-02-01", "2007-02-02")),]
 head(data2)
 
 data2$Date <- as.Date(as.character(data2$Date), format="%d/%m/%Y")
